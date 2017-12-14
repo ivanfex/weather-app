4278d82d77bf01bedb5358408ccf573271063362
Ivan Eduardo Felix Herrera <ivanfex@Ivans-MBP.home>
2017-12-13 22:42:37 -0500

    full functionality working

--- src/components/detail.js
+++ src/components/detail.js
@@ -7,13 +7,14 @@ class Detail extends Component {
 		return Math.round((temp - 273.15) * 9/5 + 32)
 	}
 	render() {
+        console.log(this.props);
 		return (
 			<div className="Detail">
 				<h3 className="Date">{this.props.location.state.date}</h3>
 				<h2>{this.props.location.state.city}</h2>
 				<h3> {this.props.location.state.data.weather[0].description} </h3>
-				<h2>min temp: {this.convert(this.props.location.state.data.main.temp_min)}</h2>
-				<h2>max temp: {this.convert(this.props.location.state.data.main.temp_max)}</h2>
+				<h2>min temp: {this.convert(this.props.location.state.data.main.temp_min)}F</h2>
+				<h2>max temp: {this.convert(this.props.location.state.data.main.temp_max)}F</h2>
 				<h2>humidity: {this.props.location.state.data.main.humidity}</h2>
 			</div>
 		)
